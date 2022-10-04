91a7a1bd160d511ac89504feb138bd3005bab59a
Quin Mundell <6mundq94@solent.ac.uk>
2022-10-04 14:10:06 +0100

    Listed employees

---
+++ Arrays/ArrayPract.py
@@ -0,0 +1,3 @@
+employees = ["John Stevenson", "Jane Smith", "Tim Wilson", "Kate Stevenson", "Kate Palmer", "Tom Eastman",
+             "Laura Green", "Mike Watson", "Sally Black", "Mark Ramsey"]
+print(employees)
